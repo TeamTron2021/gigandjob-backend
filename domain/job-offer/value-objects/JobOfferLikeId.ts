f9fb5de3a3638c88921dd9486eb5239589dd816a
superdie18 <darojas.17@est.ucab.edu.ve>
2021-12-16 18:03:45 -0400

    Creacion Value object JobOfferLikeID y de clase base de los value objects

---
+++ domain/job-offer/value-objects/JobOfferLikeId.ts
@@ -0,0 +1,20 @@
+import { ValueObject } from "../../../shared/ValueObject";
+
+interface IDProps {
+    value: string;
+  }
+  
+  export class ID extends ValueObject<IDProps> {
+    private constructor(props: IDProps) {
+      super(props);
+    }
+  
+    get value(): string {
+      return this.props.value;
+    }
+  
+    public static create(value: string): ID {
+      return new ID({ value: value });
+    }
+  // Tal vez debe ir en shared porque es general para los ID
+  }
