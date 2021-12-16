382d92f1e76f0303bf0a317526312131d88091ad
JoseChirinos36 <jlchirinos.15@est.ucab.edu.ve>
2021-12-16 12:54:48 -0400

    Agregando InterviewRejected.ts e InterviewRejected.ts

---
+++ domain/events/InterviewReject.ts
@@ -0,0 +1,8 @@
+/**
+ * Aqui va el import de las clases necesarias.
+ */
+
+export interface IDomainEvent{
+
+    
+}
---
+++ domain/events/InterviewRejected.ts
@@ -0,0 +1,12 @@
+/**
+ * Aqui iran los imports
+ * import { IDomainEvent } from "./IDomainEvent-InterviewRejected";
+ */
+
+//Interfaz IinterviewChangeStatus{changeStatus(Interview)}
+//IObservableInterview {notifyAll(), addObserver(notifyList), removeObserver(notifyList)}
+
+export class InterviewRejected{
+
+    
+}
