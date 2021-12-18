e2de19126466aae47188c0607961f616e5701617
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2021-12-18 15:07:21 -0400

    Update and rename UserConfirmed.Event.ts to UserConfirmed.event.ts

--- domain/user/domain_events/UserConfirmed.event.ts
+++ domain/user/domain_events/UserConfirmed.event.ts
@@ -1,4 +1,4 @@
-import {UserStatus} from "../enums/UserStatus.Enum";
+import {UserStatus} from "../enums/UserStatus.enum";
 import {UserID} from "../value_objects/UserID.value";
 
 export class UserConfirmed{
