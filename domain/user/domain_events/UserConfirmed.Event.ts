38c01415cc9192046afd8d593049a0794941955b
Pablo Perez <pmperez.18@est.ucab.edu.ve>
2021-12-18 10:33:38 -0400

    Revert "Merge branch 'main' into CU4-Crear-Oferta-de-Trabajo"
    This reverts commit 502c01543a78556ab51ed8321eb915d402cd054b, reversing
    changes made to f253f8e3d6e1573010a81914c087d1db895780c2.

--- domain/user/domain_events/UserConfirmed.Event.ts
+++
@@ -1,9 +0,0 @@
-import {UserStatus} from "../enums/UserStatus.Enum";
-import {UserID} from "../value_objects/UserID.value";
-
-export class UserConfirmed{
-	constructor(
-		public ID: UserID,
-		public status: UserStatus
-	){}
-}
