28bd929840172881cfc99009349c7007a0d4c8bd
Pablo Perez <pmperez.18@est.ucab.edu.ve>
2021-12-18 10:38:16 -0400

    Revert "Revert "Merge branch 'main' into CU4-Crear-Oferta-de-Trabajo""
    This reverts commit f253f8e3d6e1573010a81914c087d1db895780c2.

---
+++ domain/user/value_objects/UserID.value.ts
@@ -0,0 +1,11 @@
+import {UserIdEmpty} from "../errors/UserIdEmpty.error"
+
+export class UserID {
+
+	public readonly value: string
+
+	constructor(value: string){
+		if (!value || !value.trim()) throw new UserIdEmpty()
+		this.value = value
+	}
+}
