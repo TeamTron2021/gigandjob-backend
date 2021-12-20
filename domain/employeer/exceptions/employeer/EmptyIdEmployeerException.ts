09ab890552c052182036c1524a0041c16e7a9020
Pablo Perez <pmperez.18@est.ucab.edu.ve>
2021-12-20 11:59:20 -0400

    EmployeerId y EmployeerIndustry tests completados

--- domain/employeer/exceptions/employeer/EmptyIdEmployeerException.ts
+++ domain/employeer/exceptions/employeer/EmptyIdEmployeerException.ts
@@ -1,7 +1,9 @@
 import { IException } from "../../../../shared/domain/Iexception";
 
-export default class EmptyEmployeerIdException implements IException {
-    public constructor(public readonly message: string) {}
+export default class EmptyEmployeerIdException extends Error implements IException {
+    public constructor(public readonly message: string) {
+        super(message);
+    }
 
     public showError(): string {
         return this.message;      
--- domain/employeer/value-objects/employeer/EmployeerIndustry.ts
+++ domain/employeer/value-objects/employeer/EmployeerIndustry.ts
@@ -14,11 +14,18 @@ export default class EmployeerIndustry implements IValueObject {
     }
 
     public static create(industry:string){
-        if(industry == undefined || industry == null || !industry.trim()){
+        if(industry == undefined || industry == null){
             throw new EmployeerEmptyIndustryException('La industria no puede estar vacia')
         }
+
         if(typeof industry != "string"){
             throw new EmployeerInvalidIndustryException('La industria tiene que ser un string');
         }
+
+        if(!industry.trim()){
+            throw new EmployeerEmptyIndustryException('La industria no puede estar vacia');
+        }
+
+        return new EmployeerIndustry(industry);
     }
 }
