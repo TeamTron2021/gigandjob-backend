9212722bb41be32435e1fc15a365af326366c3b4
Valentina Figueroa <vafigueroa.15@est.ucab.edu.ve>
2021-12-21 18:53:20 -0400

    Adición de comentarios en el código

--- domain/interview/domain-services/IChangeInterviewStatus.ts
+++ domain/interview/domain-services/IChangeInterviewStatus.ts
@@ -2,7 +2,14 @@ import {DomainService} from "../../../shared/domain/DomainService";
 import {InterviewStatus} from "../shared/InterviewStatus.enum";
 
 export abstract class IChangeInterviewStatus implements DomainService{
-    constructor() {}
-
-    abstract changeStatus(interviewStatus: InterviewStatus): InterviewStatus;
+	constructor() {}
+	
+	/**
+	 * Verifica el estado actual de la entrevista y devuelve el estado deseado a asignar, solo si todas las
+	 * verificaciones han pasado exitosamente.
+	 *
+	 * @param interviewStatus Estado actual de la entrevista.
+	 * @return Nuevo estado a asignar a la entrevista.
+	 * */
+	abstract changeStatus(interviewStatus: InterviewStatus): InterviewStatus;
 }
