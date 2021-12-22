e4f423419a842c370bdcc705065f31e309d70fa9
Valentina Figueroa <vafigueroa.15@est.ucab.edu.ve>
2021-12-21 21:54:05 -0400

    TT3-D y TT4-Testing: creación de servicio de dominio ChangeInterviewStatusToAccepted y sus pruebas unitarias.

--- domain/interview/domain-services/ChangeInterviewStatusToAccepted.ts
+++ domain/interview/domain-services/ChangeInterviewStatusToAccepted.ts
@@ -1,8 +1,24 @@
 import {IChangeInterviewStatus} from "./IChangeInterviewStatus";
 import {InterviewStatus} from "../shared/InterviewStatus.enum";
+import InterviewCurrentlyDisabledException from "../exceptions/ChangeInterviewStatus/InterviewCurrentlyDisabledException";
 
-export class ChangeInterviewStatusToAccepted extends IChangeInterviewStatus{
+/**
+ * Servicio de dominio que devuelve el estado de "aceptada" para asignar a la entrevista.
+ * */
+export default class ChangeInterviewStatusToAccepted extends IChangeInterviewStatus{
+    /**
+     * Verifica el estado actual de la entrevista y devuelve el estado deseado a asignar, solo si todas las
+     * verificaciones han pasado exitosamente.
+     *
+     * @param interviewStatus Estado actual de la entrevista.
+     * @return Nuevo estado a asignar a la entrevista.
+     * @throws InterviewCurrentlyDisabledException
+     * */
     public changeStatus(interviewStatus: InterviewStatus): InterviewStatus {
+        if (interviewStatus == InterviewStatus.disabled) {
+            throw new InterviewCurrentlyDisabledException('La entrevista está actualmente deshabilitada.');
+        }
+        
         return InterviewStatus.accepted;
     }
 }
