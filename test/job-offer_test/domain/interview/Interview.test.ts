09b4727b156895069ddf21a7d4406755c5a93104
jotacee98 <juancarlosandradelopez@gmail.com>
2021-12-21 20:50:49 -0400

    tests de update

--- test/job-offer_test/domain/interview/Interview.test.ts
+++ test/job-offer_test/domain/interview/Interview.test.ts
@@ -6,8 +6,6 @@ import InterviewInterviewed from "../../../../domain/interview/value-objects/int
 import InterviewInterviewer from "../../../../domain/interview/value-objects/interview/InterviewInterviewer";
 import InterviewTitle from "../../../../domain/interview/value-objects/interview/InterviewTitle";
 import UniqueId from "../../../../shared/domain/UniqueUUID";
-import {InterviewDataUpdated} from "../../../../domain/interview/domain-events/InterviewDataUpdated.Event"
-import { title } from "process";
 
 
 describe('Testing Interview creation', ()=>{
@@ -34,38 +32,5 @@ describe('Testing Interview creation', ()=>{
         ); 
         expect(interview).toBeInstanceOf(Interview);
     })
-    test('Should update the basic Interview data',() =>{
-		const initialDate = new Date(); 
-        const finalDate = new Date(); 
-        initialDate.setDate(finalDate.getDate() -1);
-        const date = InterviewDate.create(
-            initialDate, 
-            finalDate
-        );
-
-        const id = InterviewId.create(new UniqueId().getId());
-        const interviewed = InterviewInterviewed.create(new UniqueId().getId());
-        const interviewer = InterviewInterviewer.create(new UniqueId().getId());
-        const interview = Interview.create(
-            InterviewTitle.create('Titulo generico de una entrevista'),
-            InterviewDescription.create('Descripcion generica de una entrevista de trabajo'), 
-            date, 
-            interviewed,
-            interviewer,
-            id
-        ); 
-
-		interview.updateData(
-		    new InterviewDescription("descripción actualizada de la entrevista"),
-            new InterviewTitle("Titulo actualizado de la entrevista")
-            )
-		const event = new InterviewDataUpdated(
-			interview.description,
-			interview.title,
-		)
-		expect(interview.description).toStrictEqual(new InterviewDescription("descripción actualizada de la entrevista"))
-		expect(interview.title).toStrictEqual(new InterviewTitle("Titulo actualizado de la entrevista"))
-		expect(interview.getEvents()).toContainEqual(event)
-	})
 })
 
