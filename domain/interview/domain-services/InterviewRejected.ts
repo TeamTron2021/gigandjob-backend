62acf5b447ca2a203231b51eb87f34a005db91f8
JoseChirinos36 <jlchirinos.15@est.ucab.edu.ve>
2021-12-22 07:41:13 -0400

    Modificado InterviewRejected

--- domain/interview/domain-services/InterviewRejected.ts
+++ domain/interview/domain-services/InterviewRejected.ts
@@ -1,10 +1,14 @@
 
 import {IChangeInterviewStatus} from "./IChangeInterviewStatus";
 import { InterviewStatus } from "../shared/InterviewStatus.enum";
+import InterviewCurrentlyDisabledException from "../exceptions/ChangeInterviewStatus/InterviewCurrentlyDisabledException";
 
-export class InterviewRejected implements  IChangeInterviewStatus{
+export class InterviewRejected extends  IChangeInterviewStatus{
   
     public changeStatus(interviewStatus: InterviewStatus): InterviewStatus {
+        if(interviewStatus == InterviewStatus.disabled){
+            throw new InterviewCurrentlyDisabledException('Entrevista esta deshabilitada');
+        }
         return InterviewStatus.rejected;
     }
 }
