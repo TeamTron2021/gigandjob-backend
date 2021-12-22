cda8f202cfcf02ce4da596fcb0227f1983111010
JoseChirinos36 <jlchirinos.15@est.ucab.edu.ve>
2021-12-22 10:27:39 -0400

    Cambio de nombre InterviewRejected a ChangeInterviewStatusToRejected agregado y en proceso test ChangeInterviewStatusRejected

--- domain/interview/domain-services/ChangeInterviewStatusToRejected.ts
+++ domain/interview/domain-services/ChangeInterviewStatusToRejected.ts
@@ -3,9 +3,10 @@ import {IChangeInterviewStatus} from "./IChangeInterviewStatus";
 import { InterviewStatus } from "../shared/InterviewStatus.enum";
 import InterviewCurrentlyDisabledException from "../exceptions/ChangeInterviewStatus/InterviewCurrentlyDisabledException";
 
-export class InterviewRejected extends  IChangeInterviewStatus{
+export class ChangeInterviewStatusToRejected extends  IChangeInterviewStatus{
   
     public changeStatus(interviewStatus: InterviewStatus): InterviewStatus {
+        
         if(interviewStatus == InterviewStatus.disabled){
             throw new InterviewCurrentlyDisabledException('Entrevista esta deshabilitada');
         }
---
+++ test/job-offer_test/domain/interview/domain-services/ChangeInterviewStatusToRejected.test.ts
@@ -0,0 +1,29 @@
+import {InterviewStatus} from "../../../../../domain/interview/shared/InterviewStatus.enum";
+import {IChangeInterviewStatus} from "../../../../../domain/interview/domain-services/IChangeInterviewStatus";
+import { ChangeInterviewStatusToRejected } from "../../../../../domain/interview/domain-services/ChangeInterviewStatusToRejected";
+import InterviewCurrentlyDisabledException from "../../../../../domain/interview/exceptions/ChangeInterviewStatus/InterviewCurrentlyDisabledException";
+
+describe('test to changeInterviewStatusToRejected ',()=>{
+    
+    const expectedInterviewStatus : InterviewStatus = InterviewStatus.rejected;
+
+    test('throw exception in a current status is disable',()=>{
+    
+        const interviewStatus: InterviewStatus = InterviewStatus.disabled;
+        let interviewChanger : IChangeInterviewStatus = new ChangeInterviewStatusToRejected();
+
+        expect(()=>{    
+    
+            interviewChanger.changeStatus(interviewStatus);
+            }).toThrow(new InterviewCurrentlyDisabledException('Entrevista actualmente deshabilitada'));
+    });
+
+    test('Return "rejected" interview status',()=>{
+    
+        const interviewStatus: InterviewStatus = InterviewStatus.rejected;
+        let interviewChanger : IChangeInterviewStatus = new ChangeInterviewStatusToRejected();
+
+        expect(newInteviewStatus).toBe(expectedInterviewStatus);
+    });
+
+});
