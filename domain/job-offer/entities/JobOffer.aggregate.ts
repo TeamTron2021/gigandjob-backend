2c30fc4328716046981ff01a02087678235acaa8
superdie18 <darojas.17@est.ucab.edu.ve>
2021-12-22 15:37:27 -0400

    Se agrega domain event JobOfferModified y test

--- domain/job-offer/entities/JobOffer.aggregate.ts
+++ domain/job-offer/entities/JobOffer.aggregate.ts
@@ -1,5 +1,6 @@
 import IDomainEvent from "../../../shared/domain/IDomainEvent";
 import JobOfferCreated from "../domain-events/job-offer/JobOfferCreated.Event";
+import JobOfferModified from "../domain-events/job-offer/JobOfferModified";
 import IJobOffer from "../shared/IJobOffer";
 import { OfferStatus } from "../shared/OfferStatus.enum";
 import GigDuration from "../value-objects/Gig/JobOfferGigDuration";
@@ -64,10 +65,32 @@ export default class JobOffer<S extends OfferStatus> implements IJobOffer {
         _gigDuration?: GigDuration,
     ){
         const offer = new JobOffer(description, salary,skills, title, vacant, likes, complaint, date, OfferStatus.notPublished, Id, );
-        offer.eventRecorder.push(new JobOfferCreated(Id,description,salary,skills,title,vacant,likes,date, OfferStatus.notPublished));
+        offer.eventRecorder.push(new JobOfferCreated(Id,description,salary,skills,title,vacant,likes,complaint,date, OfferStatus.notPublished));
         return offer;
     }
-
+    modified( 
+        description: JobOfferDescription,
+        salary: JobOfferSalary,
+        skills: JobOfferSkill[],
+        title: JobOfferTItle,
+        vacant: JobOfferVacant,
+        likes: JobOfferLike[],
+        complaint: JobOfferComplaint[],
+        date: JobOfferDate,
+        Id: JobOfferId,
+        _gigDuration?: GigDuration,
+    ){  
+        this.description = description,
+        this.salary = salary,
+        this.skills = skills,
+        this.title = title,
+        this.vacant = vacant,
+        this.likes = likes
+        this.complaint = complaint,
+        this.date = date,
+        this.Id = Id
+        this.eventRecorder.push(new JobOfferModified(Id,description,salary,skills,title,vacant,likes,complaint,date, OfferStatus.notPublished))
+    }
 
     protected invariants() {}
 }
