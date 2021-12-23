8da3cc8f9931899787136d9080762df5cb21994f
Valentina Figueroa <vafigueroa.15@est.ucab.edu.ve>
2021-12-23 09:02:02 -0400

    TT2-D y TT3-Testing
    Adición de método "create" a la entidad InterviewAcceptedNotification.
    Pruebas unitarias de dicha entidad.

--- domain/job-offer/entities/InterviewAcceptedNotification.ts
+++ domain/job-offer/entities/InterviewAcceptedNotification.ts
@@ -1,14 +1,33 @@
 import {InterviewNotificationSubject} from "../value-objects/Interview/InterviewNotificationSubject";
 import {InterviewNotificationContent} from "../value-objects/Interview/InterviewNotificationContent";
-import Interview from "./Interview";
-import {InterviewStatus} from "../shared/InterviewStatus.enum";
 import InterviewNotificationId from "../value-objects/Interview/interview/InterviewNotificationId";
+import UniqueId from "../../../shared/domain/UniqueUUID";
+import InterviewId from "../value-objects/Interview/interview/InterviewId";
 
 export default class InterviewAcceptedNotification /*extends InterviewNotification*/ {
-	public constructor(
+	private constructor(
 		private readonly subject: InterviewNotificationSubject,
 		private readonly content: InterviewNotificationContent,
-		private readonly interview: Interview<InterviewStatus>,
+		private readonly interview: InterviewId,
 		private readonly id: InterviewNotificationId
 	) {}
+	
+	public static create(
+		subject: InterviewNotificationSubject,
+		content: InterviewNotificationContent,
+		interview: InterviewId
+	): InterviewAcceptedNotification {
+		const id = InterviewNotificationId.create(new UniqueId().getId());
+		
+		try {
+			return new InterviewAcceptedNotification(subject, content, interview, id);
+		} catch (e) {
+			console.log(e);
+			throw e;
+		}
+	}
+	
+	public getId(): InterviewNotificationId {
+		return this.id;
+	}
 };
