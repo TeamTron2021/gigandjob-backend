6575afd8eb135545652b9ca021bbe8c20f78ab21
Joselyn Gonzalez <josygonzalez11@gmail.com>
2021-12-22 19:20:29 -0500

    Update disableInterview.ts

--- domain/job-offer/domain-service/disableInterview.ts
+++ domain/job-offer/domain-service/disableInterview.ts
@@ -1,11 +1,6 @@
 import Interview from "../entities/Interview";
 import { InterviewStatus } from "../shared/InterviewStatus.enum";
-import InterviewInterviewed from "../value-objects/interview/InterviewInterviewed";
-import InterviewInterviewer from "../value-objects/interview/InterviewInterviewer";
-import { InterviewDate } from "../value-objects/interviewDate";
-import { InterviewDescription } from "../value-objects/interviewDescription";
-import { InterviewId } from "../value-objects/interviewId";
-import { InterviewTitle } from "../value-objects/interviewTitle";
+
 
 
 export default class disableInterview{
