4dbc8758df9584b870e579488d969e3fc07d8fb8
Joselyn Gonzalez <josygonzalez11@gmail.com>
2021-12-23 16:14:04 -0500

    Update postulation.ts

--- domain/job-offer/entities/postulation.ts
+++ domain/job-offer/entities/postulation.ts
@@ -102,7 +102,7 @@ export class Postulation<S extends PostulationStatus> {
         const interviewReactivate : IChangeInterviewStatus = new ChangeInterviewStatusToEnable();
 
         this.interviews.forEach((interv)=>{
-             interv.status = interviewReactivate.changeStatus(InterviewStatus.enable);
+             interv.status = interviewReactivate.changeStatus(InterviewStatus.disabled);
         })
         
     }
