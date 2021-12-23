dc416ddc073dc318ea1ae6d9511ad593eadc5a34
Joselyn Gonzalez <josygonzalez11@gmail.com>
2021-12-23 03:27:50 -0500

    Update disabledInterview.Event.ts

--- domain/job-offer/domain-events/interview/interview/disabledInterview/disabledInterview.Event.ts
+++ domain/job-offer/domain-events/interview/interview/disabledInterview/disabledInterview.Event.ts
@@ -2,7 +2,7 @@ import IDomainEvent from "../../../../../../shared/domain/IDomainEvent";
 import { InterviewStatus } from "../../../../shared/InterviewStatus.enum";
 import InterviewId from "../../../../value-objects/Interview/interview/InterviewId";
 
-export class disabledInterviewE implements IDomainEvent{
+export default class disabledInterviewE implements IDomainEvent{
     constructor(
         public id: InterviewId,
         public status: InterviewStatus
