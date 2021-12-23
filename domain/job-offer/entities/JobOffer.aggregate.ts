a792e2eec2811789c452ad267cc6d155d4686144
Valentina Figueroa <vafigueroa.15@est.ucab.edu.ve>
2021-12-22 22:28:05 -0400

    TT13-D: Adición de método acceptOnlineInterview al aggregate root.
    Job Offer (aggregate root) puede cambiar el estado de una entrevista online.

--- domain/job-offer/entities/JobOffer.aggregate.ts
+++ domain/job-offer/entities/JobOffer.aggregate.ts
@@ -15,82 +15,128 @@ import { PostulationStatus } from "../value-objects/postulation/PostulationStatu
 import { JobOfferComplaint } from "./JobOfferComplaint";
 import { JobOfferLike } from "./JobOfferLike";
 import { Postulation } from "./postulation";
+import InterviewTitle from "../value-objects/Interview/interview/InterviewTitle";
+import InterviewDescription from "../value-objects/Interview/interview/InterviewDescription";
+import InterviewDate from "../value-objects/Interview/interview/InterviewDate";
+import InterviewInterviewed from "../value-objects/Interview/interview/InterviewInterviewed";
+import InterviewInterviewer from "../value-objects/Interview/interview/InterviewInterviewer";
+import InterviewId from "../value-objects/Interview/interview/InterviewId";
+import OnlineInterviewUrlMeeting from "../value-objects/Interview/OnlineInterview/OnlineInterviewUrlMeeting";
+import {InterviewStatus} from "../shared/InterviewStatus.enum";
+import OnlineInterview from "./OnlineInterview";
+import InterviewAccepted from "../domain-events/interview/interview/InterviewAccepted.Event";
 
 
 export default class JobOffer<S extends OfferStatus> implements IJobOffer {
-    private eventRecorder: IDomainEvent[] = [];
-    private postulations: Postulation<PostulationStatus>[] = [];
-    public status: S;
-    constructor ( 
-        public description: JobOfferDescription, 
-        public salary: JobOfferSalary, 
-        public skills: JobOfferSkill[], 
-        public title: JobOfferTItle, 
-        public vacant: JobOfferVacant, 
-        public likes: JobOfferLike[],
-        public complaint: JobOfferComplaint[],
-        public date: JobOfferDate,
-        status: S,
-        private Id: JobOfferId,){   
-        this.status =  status;
-
-    }
-    public getPostulations(): Postulation<PostulationStatus>[] {
-        return this.postulations;
-    }
-
-    public addPostulation(postulation: Postulation<PostulationStatus>): void {
-        this.postulations.push(postulation)
-    }
-    public getOfferId(){
-        return this.Id;
-    }
-    public getEvents(){
-        return this.eventRecorder;
-    }
-    public addEvent(domainEvent: IDomainEvent){
-        this.eventRecorder.push(domainEvent);
-    }
-
-    static create( 
-        description: JobOfferDescription,
-        salary: JobOfferSalary,
-        skills: JobOfferSkill[],
-        title: JobOfferTItle,
-        vacant: JobOfferVacant,
-        likes: JobOfferLike[],
-        complaint: JobOfferComplaint[],
-        date: JobOfferDate,
-        Id: JobOfferId,
-        _gigDuration?: GigDuration,
-    ){
-        const offer = new JobOffer(description, salary,skills, title, vacant, likes, complaint, date, OfferStatus.notPublished, Id, );
-        offer.eventRecorder.push(new JobOfferCreated(Id,description,salary,skills,title,vacant,likes,complaint,date, OfferStatus.notPublished));
-        return offer;
-    }
-    modified( 
-        description: JobOfferDescription,
-        salary: JobOfferSalary,
-        skills: JobOfferSkill[],
-        title: JobOfferTItle,
-        vacant: JobOfferVacant,
-        likes: JobOfferLike[],
-        complaint: JobOfferComplaint[],
-        date: JobOfferDate,
-        Id: JobOfferId,
-        _gigDuration?: GigDuration,
-    ){  
-        this.description = description,
-        this.salary = salary,
-        this.skills = skills,
-        this.title = title,
-        this.vacant = vacant,
-        this.likes = likes
-        this.complaint = complaint,
-        this.date = date,
-        this.Id = Id
-        this.eventRecorder.push(new JobOfferModified(Id,description,salary,skills,title,vacant,likes,complaint,date, OfferStatus.notPublished))
-    }
-
-    protected invariants() {}
+	private eventRecorder: IDomainEvent[] = [];
+	private postulations: Postulation<PostulationStatus>[] = [];
+	public status: S;
+	constructor (
+		public description: JobOfferDescription,
+		public salary: JobOfferSalary,
+		public skills: JobOfferSkill[],
+		public title: JobOfferTItle,
+		public vacant: JobOfferVacant,
+		public likes: JobOfferLike[],
+		public complaint: JobOfferComplaint[],
+		public date: JobOfferDate,
+		status: S,
+		private Id: JobOfferId,){
+		this.status =  status;
+		
+	}
+	public getPostulations(): Postulation<PostulationStatus>[] {
+		return this.postulations;
+	}
+	
+	public addPostulation(postulation: Postulation<PostulationStatus>): void {
+		this.postulations.push(postulation)
+	}
+	public getOfferId(){
+		return this.Id;
+	}
+	public getEvents(){
+		return this.eventRecorder;
+	}
+	public addEvent(domainEvent: IDomainEvent){
+		this.eventRecorder.push(domainEvent);
+	}
+	
+	static create(
+		description: JobOfferDescription,
+		salary: JobOfferSalary,
+		skills: JobOfferSkill[],
+		title: JobOfferTItle,
+		vacant: JobOfferVacant,
+		likes: JobOfferLike[],
+		complaint: JobOfferComplaint[],
+		date: JobOfferDate,
+		Id: JobOfferId,
+		_gigDuration?: GigDuration,
+	){
+		const offer = new JobOffer(description, salary,skills, title, vacant, likes, complaint, date, OfferStatus.notPublished, Id, );
+		offer.eventRecorder.push(new JobOfferCreated(Id,description,salary,skills,title,vacant,likes,complaint,date, OfferStatus.notPublished));
+		return offer;
+	}
+	modified(
+		description: JobOfferDescription,
+		salary: JobOfferSalary,
+		skills: JobOfferSkill[],
+		title: JobOfferTItle,
+		vacant: JobOfferVacant,
+		likes: JobOfferLike[],
+		complaint: JobOfferComplaint[],
+		date: JobOfferDate,
+		Id: JobOfferId,
+		_gigDuration?: GigDuration,
+	){
+		this.description = description,
+			this.salary = salary,
+			this.skills = skills,
+			this.title = title,
+			this.vacant = vacant,
+			this.likes = likes
+		this.complaint = complaint,
+			this.date = date,
+			this.Id = Id
+		this.eventRecorder.push(new JobOfferModified(Id,description,salary,skills,title,vacant,likes,complaint,date, OfferStatus.notPublished))
+	}
+	
+	/**
+	 * Actualiza el estado de una entrevista online a "accepted", generando un evento de dominio si el cambio fue
+	 * exitoso.
+	 * */
+	public acceptOnlineInterview(
+		interviewTitle: InterviewTitle,
+		interviewDescription: InterviewDescription,
+		interviewDate: InterviewDate,
+		interviewInterviewed: InterviewInterviewed,
+		interviewInterviewer: InterviewInterviewer,
+		interviewStatus: InterviewStatus,
+		interviewId: InterviewId,
+		interviewUrlMeeting: OnlineInterviewUrlMeeting
+	): void {
+		try {
+			const interview = new OnlineInterview(
+				interviewTitle,
+				interviewDescription,
+				interviewDate,
+				interviewInterviewed,
+				interviewInterviewer,
+				interviewStatus,
+				interviewId,
+				interviewUrlMeeting
+			);
+			
+			interview.acceptInterview();
+			const interviewAcceptedEvent: IDomainEvent
+				= new InterviewAccepted(interview.getInterviewId(), interview.getStatus());
+			this.eventRecorder.push(interviewAcceptedEvent);
+		} catch (e) {
+			console.log(e);
+			throw e;
+		}
+	}
+	
+	protected invariants() {}
 }
