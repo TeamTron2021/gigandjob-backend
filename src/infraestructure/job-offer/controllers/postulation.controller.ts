470b135384cfc2fe111f4579cb1bf31201f98d6c
Ruben Gomes <rdgomes.16@est.ucab.edu.ve>
2022-01-28 19:24:40 -0400

    Algunos arreglos

--- src/infraestructure/job-offer/controllers/postulation.controller.ts
+++ src/infraestructure/job-offer/controllers/postulation.controller.ts
@@ -1,5 +1,5 @@
 import { ApiResponse } from '@nestjs/swagger';
-import { Body, Controller, Param, Get,Post } from '@nestjs/common';
+import { Body, Controller, Param, Get, Post } from '@nestjs/common';
 import { FindJobOfferByIdRequest } from 'src/infraestructure/employeer/request/findJobOfferByID.request';
 import CreatePostulationRequest from '../request/createPostulationRequies.request';
 import { PostulationService } from '../services/postulation.service';
