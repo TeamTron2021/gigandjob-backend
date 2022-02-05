37932fd7172b0bc0da0751bdf4412c970fe30cec
Ruth Rivas <rhrivasg@gmail.com>
2022-02-05 19:25:47 -0400

    update user query model

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,22 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from 'src/application/user/User.service';
 import { UserQueryListener } from './handlers/user-query.handler';
 import { UserRegisteredHandler } from './handlers/user-registered.handler';
 import { UserCommandDao } from './user-command.dao';
-import { UserCommandEntity } from './user-command.entity';
 import { UserQueryDao } from './user-query.dao';
-import { UserQueryEntity } from './user-query.entity';
 import { UserController } from './user.controller';
 import { UserPublisher } from './user.publisher';
 import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserCommandDao, UserQueryDao]),
     CqrsModule,
+    TypeOrmModule.forFeature([UserCommandDao, UserQueryDao]),
   ],
   providers: [
     UserRepository,
