befaa82042f159f73e9897ca9238601487fd22d3
andreaamg22 <52538264+andreaamg22@users.noreply.github.com>
2022-02-10 22:40:05 -0400

    module añadido

--- src/infraestructure/job-offer/postulation.module.ts
+++ src/infraestructure/job-offer/postulation.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import AcceptPostulationService from 'src/application/job-offer/services/AcceptPostulation.service';
+import RejectPostulationService from 'src/application/job-offer/services/RejectPostulation.service';
+import RejectedPostulationService from 'src/application/job-offer/services/RejectPostulation.service';
 import { PostulationController } from './controllers/postulation.controller';
+import { AcceptpostulationHandler } from './handlers/acceptPostulation.handler';
 import { CreatePostulationHandler } from './handlers/createPostulation.handler';
 import { findPostulationByIdHandler } from './handlers/findPostulationById.handler';
+import { RejectpostulationHandler } from './handlers/RejectPostulation.handler';
 import PostulationRepository from './repositories/postulationRepository.repository';
 import { PostulationService } from './services/postulation.service';
 
@@ -14,6 +19,10 @@ import { PostulationService } from './services/postulation.service';
     PostulationService,
     CreatePostulationHandler,
     findPostulationByIdHandler,
+    AcceptpostulationHandler,
+    RejectpostulationHandler,
+    AcceptPostulationService,
+    RejectPostulationService
   ],
 })
 export class PostulationModule {}
